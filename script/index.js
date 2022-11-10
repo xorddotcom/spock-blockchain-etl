f29807bafc264a6585658b295a3da28b764b24cf
Ahmed Ali <imahmedalibhatti@gmail.com>
2022-11-10 17:29:41 +0500

    feat: script for server

---
+++ script/index.js
@@ -0,0 +1,78 @@
+const util = require("util");
+const exec = util.promisify(require("child_process").exec);
+const csv = require("csvtojson");
+const fs = require("fs");
+const axios = require("axios");
+
+const START_BLOCK = 15924338; // 15914348 (10,000) & 15923348 (1000)
+const END_BLOCK = 15924348;
+const BATCH_SIZE = 10;
+
+async function main() {
+  if (END_BLOCK - START_BLOCK <= 0) {
+    throw Error("Please give correct start and end blocks");
+  }
+
+  const totalBlocks = END_BLOCK - START_BLOCK;
+  console.log("totalBlocks: ", totalBlocks);
+
+  const cycles = totalBlocks / BATCH_SIZE;
+  console.log("cycles: ", cycles);
+
+  let startBlock = START_BLOCK;
+  let endBlock;
+
+  for (let i = 0; i < cycles; i++) {
+    endBlock = startBlock + BATCH_SIZE;
+
+    // console.log(startBlock, endBlock);
+
+    /// 1. run shell script for blocks
+    console.log(`running "shell/process.sh ${startBlock} ${endBlock}"`);
+    const { stdout, stderr } = await exec(
+      `shell/process.sh ${startBlock} ${endBlock}`
+    );
+    console.log("stdout:", stdout);
+    console.error("stderr:", stderr);
+
+    /// 2. read csv files from data folder
+
+    // 2a. list all files
+    const files = fs.readdirSync("./data");
+    // console.log("files: ", files);
+
+    // 2b. filter only csv files
+    const filteredFiles = files.filter((f) => f.includes("csv"));
+    console.log("filteredFiles: ", filteredFiles);
+
+    /// 3. convert to json file
+    let jsonFiles = {};
+    for (const filename of filteredFiles) {
+      console.log("filename: ", filename);
+      const jsonArray = await csv().fromFile(`data/${filename}`);
+      // console.log("jsonArray: ", jsonArray);
+      jsonFiles[filename] = jsonArray;
+    }
+
+    Object.keys(jsonFiles).forEach((f) => console.log(f));
+
+    console.log("-----------------------------------------------------");
+    // console.log("jsonFiles: ", jsonFiles);
+    fs.writeFileSync("data/output.json", JSON.stringify(jsonFiles, null, 2));
+
+    /// 4. send json files to server
+    await axios.post(
+      "http://172.18.0.69:5000/process/blocks",
+      JSON.stringify(jsonFiles)
+    );
+
+    startBlock = endBlock;
+  }
+}
+
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error);
+    process.exit(1);
+  });
---
+++ shell/process.sh
@@ -0,0 +1,74 @@
+#!/usr/bin/env bash
+
+# https://gist.github.com/vncsna/64825d5609c146e80de8b1fd623011ca
+set -euo pipefail
+
+# START_BLOCK=15923348 # 15914348 (10,000) & 15923348 (1000)
+# END_BLOCK=15924348
+
+# Define the input vars
+START_BLOCK=${1?Error: Please pass start block number}
+END_BLOCK=${2?Error: Please pass end block number}
+# BATCH_SIZE=${3?Error: Please pass batch size}
+
+echo "START_BLOCK: $START_BLOCK"
+echo "END_BLOCK: $END_BLOCK"
+# echo "BATCH_SIZE: $BATCH_SIZE"
+echo "TOTAL BLOCKS: $((END_BLOCK - START_BLOCK))"
+
+PROVIDER_URI="https://rpc.ankr.com/eth"
+OUTPUT_DIR="./data"
+
+### export_blocks_and_transactions
+echo "export_blocks_and_transactions"
+python3 ethereumetl.py export_blocks_and_transactions \
+  --provider-uri ${PROVIDER_URI} \
+  --start-block ${START_BLOCK} \
+  --end-block ${END_BLOCK} \
+  --blocks-output ${OUTPUT_DIR}/blocks.csv \
+  --transactions-output ${OUTPUT_DIR}/transactions.csv
+  # --batch-size ${BATCH_SIZE}
+
+### export_receipts_and_logs
+echo "export_receipts_and_logs"
+python3 ethereumetl.py extract_csv_column \
+  --input ${OUTPUT_DIR}/transactions.csv \
+  --column hash \
+  --output ${OUTPUT_DIR}/transaction_hashes.txt
+
+python3 ethereumetl.py export_receipts_and_logs \
+  --provider-uri ${PROVIDER_URI} \
+  --transaction-hashes ${OUTPUT_DIR}/transaction_hashes.txt \
+  --receipts-output ${OUTPUT_DIR}/receipts.csv \
+  --logs-output ${OUTPUT_DIR}/logs.csv
+  # --batch-size ${BATCH_SIZE}
+
+### extract_token_transfers
+echo "extract_token_transfers"
+python3 ethereumetl.py extract_token_transfers \
+  --logs ${OUTPUT_DIR}/logs.csv \
+  --output ${OUTPUT_DIR}/token_transfers.csv
+  # --batch-size ${BATCH_SIZE}
+
+### export_contracts
+echo "export_contracts"
+python3 ethereumetl.py extract_csv_column \
+  --input ${OUTPUT_DIR}/receipts.csv \
+  --column contract_address \
+  --output ${OUTPUT_DIR}/contract_addresses.txt
+
+python3 ethereumetl.py export_contracts \
+  --provider-uri ${PROVIDER_URI} \
+  --contract-addresses ${OUTPUT_DIR}/contract_addresses.txt \
+  --output ${OUTPUT_DIR}/contracts.csv
+  # --batch-size ${BATCH_SIZE}
+
+### export_tokens
+echo "export_tokens"
+python3 ethereumetl.py filter_items -i ${OUTPUT_DIR}/contracts.csv -p "item['is_erc20'] or item['is_erc721']" | \
+python3 ethereumetl.py extract_field -f address -o ${OUTPUT_DIR}/token_addresses.txt
+
+python3 ethereumetl.py export_tokens \
+  --provider-uri ${PROVIDER_URI} \
+  --token-addresses ${OUTPUT_DIR}/token_addresses.txt \
+  --output ${OUTPUT_DIR}/tokens.csv
